cfaee067f93c01f1975fc0ab1c2115f793ec1315
varunanven <varunaven3@gmail.com>
2026-08-21 22:19:54 +0530

    Added classifier model and ci

---
+++ src/model.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+from torchvision import models
+
+
+def get_model(architecture, num_classes):
+
+    if architecture == 'mobilenet_v3_small':
+        model = models.mobilenet_v3_small(weights=models.MobileNet_V3_Small_Weights.DEFAULT)
+        in_final_layer = model.classifier.in_features
+        model.classifier = nn.Linear(in_final_layer, num_classes)
+        return model
+    
+    elif architecture == 'resnet18':
+        model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
+        in_final_layer = model.fc.in_features
+        model.fc = nn.Linear(in_final_layer, num_classes)
+        return model
+
