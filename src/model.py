8a74fd80436f063f709c6b4a907e9c07b08351ad
varunanven <varunaven3@gmail.com>
2026-08-24 15:29:02 +0530

    Updated mobilenet output layer

--- src/model.py
+++ src/model.py
@@ -6,8 +6,8 @@ def get_model(architecture, num_classes):
 
     if architecture == 'mobilenet_v3_small':
         model = models.mobilenet_v3_small(weights=models.MobileNet_V3_Small_Weights.DEFAULT)
-        in_final_layer = model.classifier.in_features
-        model.classifier = nn.Linear(in_final_layer, num_classes)
+        in_final_layer = model.classifier[3].in_features
+        model.classifier[3] = nn.Linear(in_final_layer, num_classes)
         return model
     
     elif architecture == 'resnet18':
